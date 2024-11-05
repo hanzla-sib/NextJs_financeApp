e1015758d8d79eadfba24739e43f2218c30636ef
hanzla sibghat <hanzlasib24@gmail.com>
2024-11-05 22:51:08 +0100

    handling errors server actions

--- app/(auth)/login/components/login-form.js
+++ app/(auth)/login/components/login-form.js
@@ -1,10 +1,16 @@
+"use client";
 import Input from "@/components/input";
 import SubmitButton from "@/components/submit-button";
 import { login } from "@/lib/action";
-
+import { useFormState } from "react-dom";
+const initialState = {
+  message: "",
+};
 export default function LoginForm() {
+  const [state, formAction] = useFormState(login, initialState);
+
   return (
-    <form action={login} className="space-y-2">
+    <form action={formAction} className="space-y-2">
       <Input
         type="email"
         placeholder="name@example.com"
@@ -14,6 +20,13 @@ export default function LoginForm() {
       <SubmitButton type="submit" size="sm" className="w-full">
         Sign in with email
       </SubmitButton>
+      <p
+        className={`${
+          state?.error ? "text-red-500" : "text-green-500"
+        } text-sm text-center`}
+      >
+        {state?.message}
+      </p>
     </form>
   );
 }
